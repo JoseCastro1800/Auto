097454d4355987d97c0f4efa7e6d0db8ba9997cf
JoseCastro1800 <55769727+JoseCastro1800@users.noreply.github.com>
2019-09-25 15:24:41 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automovil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo productox = new Vehiculo();
+            productox.Ver_producto(productox);
+            productox.Id = 56568;
+            productox.Nombre = "Tacoma";
+            productox.Tipo = "Todo terreno";
+            productox.Marca = "Toyota";
+            productox.Annio = 2019;
+
+  
+
+
+            Console.Write(productox.Id + "-" + productox.Nombre + "-" + productox.Tipo + "-" + productox.Marca + "-" + productox.Annio);
+            Console.ReadKey();
+
+         
+        }
+    }
+
+    }
+
+       
+
+
+
